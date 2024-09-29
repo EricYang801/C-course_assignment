a419fead8e9742c024c68e88fac04eae80ad80c8
Eric Yang <jie9481@gmail.com>
2024-09-29 17:31:55 +0800

    Initial Commit

---
+++ homework/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  homework
+//
+//  Created by Eric Yang on 9/29/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
